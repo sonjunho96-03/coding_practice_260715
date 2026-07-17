56b0b270b10a41c479bd505cb500da044ed5b7c6
Junho Son-96 <goodboy3377@gmail.com>
2026-07-17 20:06:24 +0900

    Update ［PCCE 기출문제］ 6번 ／ 물 부족.py

--- 프로그래머스/0/340202. ［PCCE 기출문제］ 6번 ／ 물 부족/［PCCE 기출문제］ 6번 ／ 물 부족.py
+++ 프로그래머스/0/340202. ［PCCE 기출문제］ 6번 ／ 물 부족/［PCCE 기출문제］ 6번 ／ 물 부족.py
@@ -1,9 +1,11 @@
 def solution(storage, usage, change):
     total_usage = 0
+
     for i in range(len(change)):
-        usage = int(usage * (100 + change[i])) / 100
+        usage += usage * change[i] / 100
         total_usage += usage
+
         if total_usage > storage:
             return i
-    
+
     return -1
