57aa9b3982137a5d3360d2c58aa71c51ae6a6c6d
Junho Son-96 <goodboy3377@gmail.com>
2026-07-18 13:55:39 +0900

    Update 추억 점수.py

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,14 +1,19 @@
 def solution(name, yearning, photo):
     answer = []
 
+    # 사진 한 장씩 확인
     for one_photo in photo:
         photo_score = 0
 
+        # 사진 속 사람 한 명씩 확인
         for person in one_photo:
+
+            # 점수가 있는 사람이면 점수 더하기
             if person in name:
                 pos = name.index(person)
                 photo_score += yearning[pos]
 
+        # 사진 한 장의 최종 점수 저장
         answer.append(photo_score)
 
     return answer
